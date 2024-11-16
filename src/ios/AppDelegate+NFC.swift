423c63d088ee2da54930bdda0e0c3daa63ed8ad5
Paul Ho <paul.ho@outsystems.com>
2024-11-17 06:57:56 +0800

    refactor for Swift 5

--- src/ios/AppDelegate+NFC.swift
+++ src/ios/AppDelegate+NFC.swift
@@ -1,53 +1,26 @@
 //
 //  AppDelegate+NFC.swift
 //
-//  Created by André Gonçalves on 13/04/2020.
-//
 import CoreNFC
+import UIKit
 
-extension AppDelegate {
-    
-    override open func application(_ application: UIApplication,
-                     continue userActivity: NSUserActivity,
-                     restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
-        
-        NSLog("Extending UIApplicationDelegate")
-        
-        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb else {
-            return false
-        }
-        
-        // Confirm that the NSUserActivity object contains a valid NDEF message.
-        if #available(iOS 12.0, *) {
-            let ndefMessage = userActivity.ndefMessagePayload
-            guard ndefMessage.records.count > 0,
-            ndefMessage.records[0].typeNameFormat != .empty else {
-                return false
-            }
-            
-            // RT3079764 NOTE: The below is commented out as it is not supported in swift 5
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
-            //let nfcPluginInstance: NfcPlugin = self.viewController.getCommandInstance("NfcPlugin") as! NfcPlugin            
-            var resolved: Bool = false;
-            // NSLog(nfcPluginInstance.debugDescription);
-            //     DispatchQueue.global().async {
-            //         let waitingTimeInterval: Double = 0.1;
-            //         print("<NFC> Did start timeout")
-            //         for _ in 1...2000 { // 5?s timeout
-            //             if ( !nfcPluginInstance.isListeningNDEF ) {
-            //             Thread.sleep(forTimeInterval: waitingTimeInterval)
-            //             } else {
-            //                 let jsonDictionary = ndefMessage.ndefMessageToJSON()
-            //                 nfcPluginInstance.sendThroughChannel(jsonDictionary: jsonDictionary)
-            //                 resolved = true
-            //                 return
-            //             }
-            //         }
-            // }
-            return resolved
-            
-        } else {
-            return false
-        }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
     }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
 }
